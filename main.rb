8527a4f738bdd4ccc1612e7c586679e977c05b3e
Kenny Ortega <kennyaoc@gmail.com>
2021-04-21 14:35:40 -0400

    Revert "My each with index method"

--- main.rb
+++ main.rb
@@ -1,13 +1,1 @@
-module Enumerable
-  def my_each
-    size.times do |i|
-      yield self[i]
-    end
-  end
-
-  def my_each_with_index
-    size.times do |i|
-      yield self[i], i
-    end
-  end
-end
+puts 'This is template of Ruby for projects in Microverse'
