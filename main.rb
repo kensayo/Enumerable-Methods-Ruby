ecca8b87bdfcf682a16e94a8273b8daa6d9693ab
Kenny Ortega <kennyaoc@gmail.com>
2021-04-21 14:33:01 -0400

    Completed my-each-with-index
    Completed the function #my_each_with_index

--- main.rb
+++ main.rb
@@ -4,4 +4,14 @@ module Enumerable
       yield self[i]
     end
   end
+
+  def my_each_with_index
+    size.times do |i|
+        yield self[i], i
+    end
 end
+end
+
+fruits = ["apple", "banana", "strawberry", "pineapple"]
+
+fruits.my_each_with_index { |fruit, index| puts fruit if index.even? }
