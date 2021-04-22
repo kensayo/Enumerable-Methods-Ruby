4d9645b5fc7ee7c048e97343db653f8ac6c9428b
Kenny Ortega <kennyaoc@gmail.com>
2021-04-22 19:24:26 -0400

    Remove comments Fixed linter
    Removed commets, added Hash support for #my_select, support for emptu blocks in #my_all, fixed linter errors

--- main.rb
+++ main.rb
@@ -1,7 +1,8 @@
 module Enumerable
   # my_each method
   def my_each
-    return to_enum(:my_each) unless block_given?    
+    return to_enum(:my_each) unless block_given?
+
     to_a.size.times do |i|
       yield to_a[i]
     end
@@ -11,30 +12,33 @@ module Enumerable
   # my_each_with_index
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
+
     to_a.size.times do |i|
-      yield self.to_a[i], i
+      yield to_a[i], i
     end
     self
   end
 
   # my_select method
   def my_select
+    return to_enum(:my_select) unless block_given?
+
     arr = []
     to_a.size.times do |i|
-      arr.push(self[i]) if yield self[i]
+      arr.push(to_a[i]) if yield to_a[i]
     end
+    return arr.to_h if is_a? Hash
+
     arr
   end
 
   # my_all? method
-  def my_all?    
+  def my_all?
+    return true unless block_given?
+
     to_a.size.times do |i|
-      if !yield self.to_a[i]
-        return false
-      end
+      return false unless yield to_a[i]
     end
     true
   end
-
 end
-
