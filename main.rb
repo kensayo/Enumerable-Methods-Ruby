68c1ec5239cb86dde407ab10b5f90f1744e03b8e
Mario Rodriguez <mariordgez@hotmail.com>
2021-04-23 09:48:48 -0500

    Added my_any function

--- main.rb
+++ main.rb
@@ -1,1 +1,44 @@
-puts 'This is template of Ruby for projects in Microverse'
+module Enumerable
+  # my_each method
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    to_a.size.times do |i|
+      yield to_a[i]
+    end
+    self
+  end
+
+  # my_each_with_index
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    to_a.size.times do |i|
+      yield to_a[i], i
+    end
+    self
+  end
+
+  # my_select method
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    arr = []
+    to_a.size.times do |i|
+      arr.push(to_a[i]) if yield to_a[i]
+    end
+    return arr.to_h if is_a? Hash
+
+    arr
+  end
+
+  # my_all? method
+  def my_all?
+    return true unless block_given?
+
+    to_a.size.times do |i|
+      return false unless yield to_a[i]
+    end
+    true
+  end
+end
