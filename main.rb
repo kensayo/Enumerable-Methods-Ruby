47b478638cced989a2dad2efe69c210ead2110ed
Mario Rodriguez <mariordgez@hotmail.com>
2021-04-23 14:45:56 -0500

    Added args functionality to my_any,all,none methods

--- main.rb
+++ main.rb
@@ -33,7 +33,17 @@ module Enumerable
   end
 
   # my_all? method
-  def my_all?
+  def my_all? (args=nil)
+    case args
+    when Regexp
+      to_a.my_each do |var|
+        return false unless args.match(var)
+      end
+    when Class
+      to_a.my_each do |var|
+        return false unless var.instance_of?(args)
+      end
+    else  
     return true unless block_given?
 
     to_a.size.times do |i|
@@ -42,33 +52,73 @@ module Enumerable
     true
   end
 
-  def my_any?
-    return true unless block_given?    
-    my_each do |var|
-     return true if yield var
+  # my_any? Method
+  def my_any?(args = nil)
+    case args
+    when Regexp
+      to_a.my_each do |var|
+        return true if args.match(var)
+      end
+    when Class
+      to_a.my_each do |var|
+        return true if var.instance_of?(args)
+      end
+    else
+      return true unless block_given?
+
+      to_a.my_each do |var|
+        return true if yield var
+      end
     end
     false
   end
 
-  def my_none?
-    return false unless block_given?  
+  # my_none? Method
+  def my_none?(args=nil)
+    case args
+    when Regexp
+      to_a.my_each do |var|
+        return false if args.match(var)
+      end
+    when Class
+      to_a.my_each do |var|
+        return false if var.instance_of?(args)
+      end
+    else  
+    return false unless block_given?
+
     my_each do |var|
-     return false if yield var
+      return false if yield var
     end
     true
   end
 
-  def my_count
+  # my_count Method
+  def my_count (num=nil)
+    return to_a.size unless block_given?
     counter=0
+    if num
+      my_each do 
+        |var| 
+        if var == num
+          counter+=1
+        end
+      end
+
+    else
     my_each do |var|
       counter+=1 if yield var
     end
+  end
     counter
   end
 
+  # my_map Method
   def my_map
+    return to_enum(:my_map) unless block_given?
     mp_new=[]
     my_each { |var| mp_new.push(yield(var))}
     mp_new
   end
+
 end
